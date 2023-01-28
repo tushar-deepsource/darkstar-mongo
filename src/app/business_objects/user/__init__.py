0565442b66e79e27c3839498cd355e961ed44325
Pedro Guzmán <pedro@subvertic.com>
2023-01-28 16:31:11 -0600

    Complete user model

--- src/app/business_objects/user/__init__.py
+++ src/app/business_objects/user/__init__.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import uuid
+from typing import Optional, List
 
 from app.business_objects.user.repository import Users
 from pydantic import BaseModel, Field
@@ -20,5 +21,56 @@ class User(BaseModel):
         None,
         title='Internal Identifier'
     )
+
+    uid: str = Field(
+        str(uuid.uuid4()),
+        title="Unique identifier of the user"
+    )
+
+    phash: str = Field(
+        None,
+        title="Password hash"
+    )
+
+    salt: str = Field(
+        None,
+        title="Password salt"
+    )
+
+    name: str = Field(
+        None,
+        title="Name of the user"
+    )
+
+    last_name: str = Field(
+        None,
+        title="Last name of the user"
+    )
+
+    disabled: bool = Field(
+        True,
+        title="Weather the user is disabled or not. Disabled by default"
+    )
+
+    claims: Optional[List[str]] = Field(
+        [
+            'authenticate'
+        ],
+        title='User claims'
+    )
+
+    # -----------------------------------------------------
+    # METHOD CAN
+    # -----------------------------------------------------
+    def can(self, claim: str) -> bool:
+        """
+        Validates if a particular claim is associated with
+        given user
+        :param claim: The claim to be verified
+        :return: True is the user has the claim. False if the
+        user does not have the claim
+        """
+        return claim in self.claims
+
 
 
